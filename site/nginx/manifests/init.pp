84ce83faf5e83bc3f441c0a0774f52f7db35ab5d
Alan Petersen <alan@puppetlabs.com>
2016-04-27 10:03:14 -0700

    Lab 13.1

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -2,52 +2,48 @@
 class nginx {
 
   $docroot = '/var/www'
-  
+  $pkg = 'nginx'
+  $svc = 'nginx'
+  $nginx_configdir = '/etc/nginx'
+  $source_base = 'puppet:///modules/nginx'
+
+  File {
+    ensure => file,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+  }
+
   # PACKAGE
-  package { 'nginx':
+  package { $pkg:
     ensure => present,
   }
 
   # DOCROOT
   file { $docroot:
     ensure => directory,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0755',
   }
 
   # WEB PAGE
-  file { '/var/www/index.html':
-    ensure => file,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0644',
-    source => 'puppet:///modules/nginx/index.html',
+  file { "${docroot}/index.html":
+    source => "${source_base}/index.html",
   }
 
   # CONFIG FILES
-  file { '/etc/nginx/nginx.conf':
-    ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    source  => 'puppet:///modules/nginx/nginx.conf',
-    require => Package['nginx'],
-    notify  => Service['nginx'],
+  file { "${nginx_configdir}/nginx.conf":
+    source  => "${source_base}/nginx.conf",
+    require => Package[$pkg],
+    notify  => Service[$pkg],
   }
 
-  file { '/etc/nginx/conf.d/default.conf':
-    ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    source  => 'puppet:///modules/nginx/default.conf',
-    require => Package['nginx'],
-    notify  => Service['nginx'],
+  file { "${nginx_configdir}/conf.d/default.conf":
+    source  => "${source_base}/default.conf",
+    require => Package[$pkg],
+    notify  => Service[$pkg],
   }
 
   # SERVICE
-  service { 'nginx':
+  service { $svc:
     ensure => running,
     enable => true,
   }
