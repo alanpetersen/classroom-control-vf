5b80499ce4e4abd58b8e9b207f5731c0c7fb9840
Alan Petersen <alan@puppetlabs.com>
2016-04-28 16:37:15 -0700

    removed domain because it wasn't working

--- site/profile/manifests/wordpress.pp
+++ site/profile/manifests/wordpress.pp
@@ -34,13 +34,11 @@ class profile::wordpress {
   # MANAGE wordpress
   class { 'wordpress':
     #install_url => 'https://wordpress.org/wordpress-3.8.tar.gz',
-    install_dir    => '/var/wordpress',
-    wp_owner       => 'wordpress',
-    wp_group       => 'wordpress',
-    db_user        => 'wordpress',
-    db_password    => 'wp_password',
-    wp_multisite   => true,
-    wp_site_domain => 'alanpetersen.puppetlabs.vm',
+    install_dir => '/var/wordpress',
+    wp_owner    => 'wordpress',
+    wp_group    => 'wordpress',
+    db_user     => 'wordpress',
+    db_password => 'wp_password',
   }
 
   # BEER O'CLOCK!
